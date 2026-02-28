f16755f5be0cceba7dccd080754fb396823460d5
jake-brennan714 <jake.brennan714@gmail.com>
2026-02-28 12:31:57 -0500

    sensor boilerplate

--- soil_hydrometer/soil_hydrometer.ino
+++ soil_hydrometer/soil_hydrometer.ino
@@ -1,13 +1,13 @@
+#define SENSOR 0
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(57600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    int incomingByte = Serial.read();
-    Serial.print("Received: ");
-    Serial.println(incomingByte);
-  }
+  Serial.print("Moisture Sensor Value:");
+  Serial.println(analogRead(SENSOR));
+  delay(100);
 }
